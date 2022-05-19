5cb03bba26b759d35103fa117ed0ec77cc61f399
Omar Ahdab <brightside996@gmail.com>
2022-05-19 13:25:38 +0300

    shape the interface for library users

--- lib/src/share_service_base.dart
+++ lib/src/share_service_base.dart
@@ -1,6 +1,22 @@
-// TODO: Put public facing types in this file.
+import 'dart:io';
+import 'dart:typed_data';
 
-/// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+
+class ShareService {
+  late List<String> _files;
+  InternetAddress _host_address = InternetAddress.anyIPv4;
+  late int _port;
+  ShareService(this._files, {dynamic address = "0.0.0.0", int port = 8080}) {
+    _port = port;
+    if (address is String) {
+      _host_address = InternetAddress(address);
+    } else if (address is InternetAddress) {
+      _host_address = address;
+    }
+  }
+
+  void run() async {}
 }
